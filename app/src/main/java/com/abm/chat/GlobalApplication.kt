b56424d89086cd827f3292bbd3361fb15210f573
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2023-09-14 15:58:57 +0900

    Feat: 구글 로그인 구현 #15

--- app/src/main/java/com/abm/chat/GlobalApplication.kt
+++ app/src/main/java/com/abm/chat/GlobalApplication.kt
@@ -1,10 +1,10 @@
 package com.abm.chat
 
 import android.app.Application
+import com.abm.chat.BuildConfig.KAKAO_API_KEY
 import com.kakao.sdk.common.KakaoSdk
 
 class GlobalApplication : Application() {
-    val KAKAO_API_KEY = BuildConfig.KAKAO_API_KEY
     companion object {
     }
     override fun onCreate() {
